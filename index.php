428406e1be852f507053daee33d124fbe3afbc6a
Josh12903 <hank76421@gmail.com>
2026-02-08 14:56:25 +0800

    登出入路徑 落實格式

--- index.php
+++ index.php
@@ -48,11 +48,11 @@
                         <?php if(isset($_SESSION['login'])):?>
                             <?php if($_SESSION['login']=='admin'):?>
                                 歡迎，<?=$_SESSION['login'];?>
-                                <button>管理</button>
-                                <button>登出</button>
+                                <button onclick="location.href='back.php'">管理</button>
+                                <button onclick="location.href='signout.php'">登出</button>
                             <?php else:?>
                                 歡迎，<?=$_SESSION['login'];?>
-                                <button>登出</button>
+                                <button onclick="location.href='signout.php'">登出</button>
                             <?php endif;?>
                         <?php else:?>
                             <a href="?do=login">會員登入</a>
