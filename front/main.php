4db9eb586e2eeaef73c15841b8feedd1c0ce8045
Josh12903 <hank76421@gmail.com>
2026-02-09 01:34:22 +0800

    前台main 建 主要內容區 與基本頁籤效果

--- front/main.php
+++ front/main.php
@@ -1,9 +1,20 @@
 <style>
 .tags{
-
+    display: flex;
+    margin-left:-1px;
 }
-.tag{
-
+.tags li{
+    list-style: none;
+    padding:5px;
+    width:100px;
+    border:1px solid #666;
+    text-align: center;
+    margin-left:-1px;
+    cursor:pointer;
+    backgroud:#eee;
+}   
+.tags li.active{
+    backgroud:skyblue;
 }
 </style>
 <ul class="tags">
@@ -13,4 +24,36 @@
     <li class="tag">慢性病防治</li>
 </ul>
 
-<div class="article"></div>
+<div class="article">
+    <div class="artcles">
+        <h2>健康新知</h2>
+        <pre>
+            
+        </pre>
+    </div>
+    <div class="artcles">
+        <h2>菸害防制</h2>
+        <pre>
+            
+        </pre>
+    </div>
+    <div class="artcles">
+        <h2>癌症防治</h2>
+        <pre>
+            
+        </pre>
+    </div>
+    <div class="artcles">
+        <h2>慢性病防治</h2>
+        <pre>
+            
+        </pre>
+    </div>
+</div>
+
+<script>
+    $(".tag").on("click",function(){
+        $(".tag").removeClass("active")
+        $(this).addClass("active")
+    })
+</script>
